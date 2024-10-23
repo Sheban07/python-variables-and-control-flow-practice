0add2b4d4a8a82fcff303cfd74d9d66518c4fa64
Sheban07 <shebankaingu@gmail.com>
2024-10-23 14:48:46 +0300

    test

---
+++ question1.py
@@ -0,0 +1,19 @@
+#Write a program that assigns two numbers to variables a and b. Perform the following
+#operations and print the results:
+#● Addition
+#● Subtraction
+#● Multiplication
+#● Division
+
+a = 60
+b = 30
+
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print("This is addition: ",add)
+print("This is subtraction: ",sub)
+print("This is multiplication: ",mul)
+print("This is division: ",div)
